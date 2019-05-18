6f22d8fc1d8d9e9fbeb4a7bf671c752079627b8c
Vitor Leonel <falecom@vitorleonel.com>
2019-05-18 17:13:11 -0300

    removendo estilos sem uso

--- src/components/App/styles.js
+++ src/components/App/styles.js
@@ -1,21 +1,12 @@
 import styled from "styled-components/native";
 import { getStatusBarHeight } from "react-native-status-bar-height";
-import { ListItem, Overlay } from "react-native-elements";
+import { ListItem } from "react-native-elements";
 
 export const Container = styled.View`
   flex: 1;
   justify-content: center;
   padding-top: ${getStatusBarHeight(true) + 8}px;
   background-color: #0097e6;
-`;
-
-export const Title = styled.Text`
-  font-size: 24px;
-  font-weight: bold;
-  color: #ffffff;
-  margin-bottom: 16px;
-  padding-left: 24px;
-  padding-right: 24px;
 `;
 
 export const Heroes = styled.FlatList``;
@@ -38,5 +29,3 @@ export const Hero = styled(ListItem).attrs({
     numberOfLines: 1
   }
 })``;
-
-export const Modal = styled(Overlay)``;
